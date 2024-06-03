ed3785c9e4ee85603dc44290ddf500f0a771ade9
adrianna-yixin <esthergong499@gmail.com>
2024-06-04 00:05:47 +0100

    Adjust the style to make the website responsive

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
- 
+//  Unplash API
+const count = 30;
+const apiKey = '6d34AzjplnXv3gfUCjmlwMJHMLq8oDjZItNfnxQKLow';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const date = await response.json();
+    } catch (error) {
+        
+    }
+}
+
+getPhotos();
